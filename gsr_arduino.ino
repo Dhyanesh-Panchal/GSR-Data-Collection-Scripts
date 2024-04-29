9af1c6fef3e026ea42cd9ef7800af86230afddde
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2024-04-29 13:50:20 +0530

    Collected Data

--- gsr_arduino.ino
+++ gsr_arduino.ino
@@ -1,26 +1,26 @@
-const int LED=13;
-const int GSR=A2;
+const int LED = 13;
+const int GSR = A2;
 float sensorValue;
 float timeKeeper = 0.0;
 int frequency;
-int maxLine=700,minLine=0,index=0;
+int maxLine = 700, minLine = 0, index = 0;
 
-
-void setup(){
-  Serial.begin(9600);
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,LOW);
+void setup()
+{
+  Serial.begin(2000000);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
   delay(1000);
   // Serial.print("starting!\ntime = ");
   // Serial.println(micros());
 }
 
+void loop()
+{
 
-void loop(){
-  
-// micros() Is used to collect the current time
+  // micros() Is used to collect the current time
 
-// frequency counting mechanism
+  // frequency counting mechanism
   // if(micros() - timeKeeper > 1000000){
   //     Serial.print(frequency);
   //     Serial.print('\t');
@@ -29,13 +29,12 @@ void loop(){
   //     timeKeeper = micros();
   //   }
 
-
-  sensorValue=analogRead(GSR);
-  index+=1;
+  sensorValue = analogRead(GSR);
+  index += 1;
   // frequency+=1;
   // Serial.print("sensorValue=");
   // Serial.print(index);
-  
+
   // Serial.print(maxLine);
   // Serial.print('\t');
   // Serial.print(minLine);
